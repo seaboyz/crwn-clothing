cbea2a06bd444ffeba8f30bfbab2556a682f2ca5
seaboyz <seaboyz@hotmail.com>
2022-01-27 12:18:21 -0600

    Finish header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 
 function App() {
 	return (
 		<div className='App'>
+			<Header />
 			<Routes>
 				<Route path='/' element={<HomePage />} />
 				<Route path='/shop' element={<ShopPage />} />
