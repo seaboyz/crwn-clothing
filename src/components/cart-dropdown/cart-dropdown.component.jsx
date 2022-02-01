77f7d051c2122dafc7d4815d8a81de60aae23d1b
seaboyz <seaboyz@hotmail.com>
2022-02-01 10:32:56 -0600

    Toogle cart hidden after go to checkout page

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -4,20 +4,39 @@ import { connect } from 'react-redux';
 import CartItem from '../cart-item/cart-item.component';
 import { selectCartItems } from '../../redux/cart/cart.selector';
 import { createStructuredSelector } from 'reselect';
+import { isEmpty } from 'ramda';
+import { useNavigate } from 'react-router-dom';
+import { toggleCartHidden } from '../../redux/cart/cart.actions.js';
 
-const CartDropdown = ({ cartItems }) => (
-	<div className='cart-dropdown'>
-		<div className='cart-items'>
-			{cartItems.map(item => (
-				<CartItem key={item.id} item={item} />
-			))}
+const CartDropdown = ({ cartItems, toggleCartHidden }) => {
+	const navigate = useNavigate();
+	return (
+		<div className='cart-dropdown'>
+			{isEmpty(cartItems) ? (
+				<span className='empty-message'>Your cart is empty</span>
+			) : (
+				<div className='cart-items'>
+					{cartItems.map(item => (
+						<CartItem key={item.id} item={item} />
+					))}
+				</div>
+			)}
+			<CustomButton
+				onClick={() => {
+					navigate('/checkout');
+					toggleCartHidden();
+				}}
+			>
+				go to checkout
+			</CustomButton>
 		</div>
-		<CustomButton>go to checkout</CustomButton>
-	</div>
-);
-
+	);
+};
 const mapStateToProps = createStructuredSelector({
 	cartItems: selectCartItems
 });
+const mapDispatchToProps = dispatch => ({
+	toggleCartHidden: () => dispatch(toggleCartHidden())
+});
 
-export default connect(mapStateToProps)(CartDropdown);
+export default connect(mapStateToProps, mapDispatchToProps)(CartDropdown);
