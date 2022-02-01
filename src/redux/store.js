de3738d83cedd6399571fae185d931bd94b63743
seaboyz <seaboyz@hotmail.com>
2022-02-01 13:40:38 -0600

    Add redux devtools entenstions

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,14 @@
-import { applyMiddleware, createStore } from 'redux';
+import { applyMiddleware, createStore, compose } from 'redux';
 import rootReducer from './root.reducer';
 import logger from 'redux-logger';
 
 const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+	rootReducer,
+	composeEnhancers(applyMiddleware(...middlewares))
+);
 
 export default store;
