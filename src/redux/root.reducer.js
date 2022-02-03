3f16238f2128090db3adbbe24a8c15f8b92e361e
seaboyz <seaboyz@hotmail.com>
2022-02-03 09:26:15 -0600

    Add directory slice and change direcotry component to function component

--- src/redux/root.reducer.js
+++ src/redux/root.reducer.js
@@ -2,13 +2,15 @@ import { combineReducers } from 'redux';
 
 import userReducer from './user/user.slice';
 import cartReducer from './cart/cart.slice';
+import directoryReducer from './directory/directory.slice';
 
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
 const rootReducer = combineReducers({
 	user: userReducer,
-	cart: cartReducer
+	cart: cartReducer,
+	directory: directoryReducer
 });
 
 const persistConfig = {
