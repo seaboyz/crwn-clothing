7ffc40eda6ef2cfd618347629d0c8e968f716d80
seaboyz <seaboyz@hotmail.com>
2022-02-07 17:53:44 -0600

    Create all user actions

--- src/redux/user/user.slice.js
+++ src/redux/user/user.slice.js
@@ -3,15 +3,49 @@ import { createSlice } from '@reduxjs/toolkit';
 export const slice = createSlice({
 	name: 'user',
 	initialState: {
-		currentUser: null
+		currentUser: null,
+		status: 'idle',
+		error: null
 	},
 	reducers: {
-		setCurrentUser: (state, action) => {
+		setCurrentUser(state, action) {
 			state.currentUser = action.payload;
+		},
+		signInWithGoogleStart(state, action) {
+			state.status = 'loading';
+		},
+		signInWithGoogleSuccess(state, action) {
+			state.status = 'succeeded';
+			state.error = null;
+			state.currentUser = action.payload;
+		},
+		signInWithGoogleFailed(state, action) {
+			state.status = 'failed';
+			state.error = action.error.message;
+		},
+		signInWithWithEmailAndPasswordStart(state, action) {
+			state.status = 'loading';
+		},
+		signInWithWithEmailAndPasswordSuccess(state, action) {
+			state.status = 'succeeded';
+			state.error = null;
+			state.currentUser = action.payload;
+		},
+		signInWithWithEmailAndPasswordFailed(state, action) {
+			state.status = 'failed';
+			state.error = action.error.message;
 		}
 	}
 });
 
-export const { setCurrentUser } = slice.actions;
+export const {
+	setCurrentUser,
+	signInWithGoogleStart,
+	signInWithGoogleSuccess,
+	signInWithGoogleFailed,
+	signInWithWithEmailAndPasswordStart,
+	signInWithWithEmailAndPasswordSuccess,
+	signInWithWithEmailAndPasswordFailed
+} = slice.actions;
 
 export default slice.reducer;
