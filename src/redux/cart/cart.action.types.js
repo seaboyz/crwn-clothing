443eba03749092acfbc3fdf25f6743bbd675eb4b
seaboyz <seaboyz@hotmail.com>
2022-02-08 15:19:16 -0600

    Clear out useless actions and reducers files

--- src/redux/cart/cart.action.types.js
+++
@@ -1,7 +0,0 @@
-export const TOGGLE_CART_HIDDEN = 'TOGGLE_CART_HIDDEN';
-
-export const ADD_ITEM = 'ADD_ITEM';
-export const REMOVE_ITEM = 'REMOVE_ITEM';
-
-export const INC_QUANTITY = 'INC_QUANTITY';
-export const DEC_QUANTITY = 'DEC_QUANTITY';
--- src/redux/user/user.actions.js
+++
@@ -1,5 +0,0 @@
-import { SET_CURRENT_USER } from './user.actions.types';
-export const setCurrentUser = user => ({
-	type: SET_CURRENT_USER,
-	payload: user
-});
