ee13508c698bc7c3bab212a16f18dc70e804afc6
seaboyz <seaboyz@hotmail.com>
2022-02-13 10:12:03 -0600

    Add lazy loading

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,19 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/header/header.component';
-import HomePage from './pages/home/home.page';
-import ShopPage from './pages/shop/shop.page.jsx';
-import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.page.jsx';
-import CheckOutPage from './pages/checkout/checkout.page';
-import CollectionPage from './pages/collection/collection.page';
-import { useEffect } from 'react';
+
+import { lazy, useEffect, Suspense } from 'react';
 import { useDispatch } from 'react-redux';
 import { checkUserSessionStart } from './redux/user/user.slice';
+import { Spinner } from './components/with-spinner/with-spiner.component';
+
+const HomePage = lazy(() => import('./pages/home/home.page.jsx'));
+const SignInAndSignUp = lazy(() =>
+	import('./pages/sign-in-and-sign-up/sign-in-and-sign-up.page.jsx')
+);
+const ShopPage = lazy(() => import('./pages/shop/shop.page.jsx'));
+const CheckOutPage = lazy(() => import('./pages/checkout/checkout.page'));
+const CollectionPage = lazy(() => import('./pages/collection/collection.page'));
 
 const App = () => {
 	const dispatch = useDispatch();
@@ -18,14 +23,16 @@ const App = () => {
 	return (
 		<div className='App'>
 			<Header />
-			<Routes>
-				<Route path='/' element={<HomePage />} />
-				<Route path='/shop' element={<ShopPage />} />
-				<Route path='/shop/:category' element={<CollectionPage />} />
-				<Route path='/signin' element={<SignInAndSignUp />} />
-				<Route path='/checkout' element={<CheckOutPage />} />
-				<Route path='*' element={<HomePage />} />
-			</Routes>
+			<Suspense fallback={<Spinner />}>
+				<Routes>
+					<Route path='/' element={<HomePage />} />
+					<Route path='/shop' element={<ShopPage />} />
+					<Route path='/shop/:category' element={<CollectionPage />} />
+					<Route path='/signin' element={<SignInAndSignUp />} />
+					<Route path='/checkout' element={<CheckOutPage />} />
+					<Route path='*' element={<HomePage />} />
+				</Routes>
+			</Suspense>
 		</div>
 	);
 };
